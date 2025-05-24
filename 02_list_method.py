67944249742e70b95fba1b9e4ac15c7997504150
“Arpita <arpitakumari10653@gmail.com>
2025-05-24 14:53:56 +0530

    using different list method

---
+++ 02_list_method.py
@@ -0,0 +1,10 @@
+friends = ["Apple","Orange",5,345.06,"Aakash","Rohan"]
+print(friends)
+
+friends.append("dog")     # use this cmd to add dog at end
+print(friends)
+
+l1 = [1,23,6,8,12,45]
+l1.sort()       # By using this cmd we arranged the list in order
+print(l1)
+

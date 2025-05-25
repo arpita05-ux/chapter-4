de9c7d3630425e75d4772c5644d112afca4f6e57
“Arpita <arpitakumari10653@gmail.com>
2025-05-25 09:55:27 +0530

    use of tuple methods

---
+++ 04_tuple_method.py
@@ -0,0 +1,11 @@
+a = (1,2,5,6,False,2,"ram","sleep")
+print(type(a))
+print(a)
+
+no = a.count(2) #by this count tuple it count no. of 2 present
+
+print(no)
+
+i = a.index(2) #index find 2 in this tuple it doesn't find another 2 is present or not
+print(i)
+
